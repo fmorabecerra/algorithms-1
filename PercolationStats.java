f139a85ed5710c84df7b3b2d92a09ed95ee72582
Pancho <fmorabecerra@gmail.com>
2019-10-13 14:48:27 -0500

    Stats work: additional framwork layout for package.

--- PercolationStats.java
+++ PercolationStats.java
@@ -4,11 +4,20 @@
  *  Description: Coursera
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdOut;
+
 public class PercolationStats {
+    private double[] percolationThreshold;
 
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
         this.validateTrialsAndN(n, trials);
+        this.percolationThreshold = new double[trials];
+
+        // Start the test loop.
+        for (int trial = 0; trial < trials; trial++) {
+            percolationThreshold[i] = 0.0;
+        }
     }
 
     // sample mean of percolation threshold
@@ -33,6 +42,14 @@ public class PercolationStats {
 
     // test client (see below)
     public static void main(String[] args) {
+        if (args.length == 2) {
+            PercolationStats percStats = new PercolationStats(Integer.parseInt(args[0]),
+                                                              Integer.parseInt(args[1]));
+            // Print mean
+            StdOut.println(percStats.mean());
+            // Print stdDev
+            // Print confidence interval.
+        }
     }
 
     private void validateTrialsAndN(int n, int trials) {
