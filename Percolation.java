0ebf02c14689500d15f16a09c689dff04313ef60
Pancho <fmorabecerra@gmail.com>
2019-10-12 22:04:18 -0500

    Add comments. Address indexing of array.

--- Percolation.java
+++ Percolation.java
@@ -9,25 +9,30 @@ import edu.princeton.cs.algs4.StdRandom;
 
 public class Percolation {
     private int gridN;
-    private int[][] percolationGrid; // Set element to 1 if open
+    private int[][] percolationGrid; // Set element to 1 if open. Zero if closed.
+    private int numOfOpenSites;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
         this.gridN = n;
         this.percolationGrid = new int[n][n]; // By default all elements are initialized to zero
+        this.numOfOpenSites = 0;
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
         checkRowAndCol(row, col);
-        this.percolationGrid[row - 1][col - 1] = 1; // You need to address this indexing issue.
+        if (this.percolationGrid[row - 1][col - 1] == 0) {
+            this.percolationGrid[row - 1][col - 1] = 1; // Maybe make a data structure for index.
+            this.numOfOpenSites += 1;
+        }
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
         checkRowAndCol(row, col);
         // Return whether element in grid is open (equals 1)
-        return (this.percolationGrid[row][col] == 1);
+        return (this.percolationGrid[row - 1][col - 1] == 1);
     }
 
     // is the site (row, col) full?
@@ -38,7 +43,7 @@ public class Percolation {
 
     // returns the number of open sites
     public int numberOfOpenSites() {
-        return 0;
+        return this.numOfOpenSites;
     }
 
     // does the system percolate?
@@ -57,10 +62,10 @@ public class Percolation {
     }
 
     private void checkRowAndCol(int row, int column) {
-        if (row < 0 || row > gridN) {
+        if (row < 1 || row > gridN) {
             throw new IllegalArgumentException(Integer.toString(row));
         }
-        if (column < 0 || column > gridN) {
+        if (column < 1 || column > gridN) {
             throw new IllegalArgumentException(Integer.toString(column));
         }
     }
