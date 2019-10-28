920600e065dcb5a9b9b1069b029ad8cab92f7a32
Pancho <fmorabecerra@gmail.com>
2019-10-28 14:09:15 -0500

    First complete version of Fast CollinearPoints.

--- algs1_week3/FastCollinearPoints.java
+++ algs1_week3/FastCollinearPoints.java
@@ -6,18 +6,20 @@
 
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class FastCollinearPoints {
-    private LineSegment[] segs;
+    private int totalSegments;
+    private final LineSegment[] validLineSegments;
 
     // finds all line segments containing 4 or more points
     public FastCollinearPoints(Point[] points) {
         validatePoints(points);
 
-        // Sort by natural order. Uses comparables
-        // Arrays.sort(points);
+        ArrayList<LineSegment> lineSegmentsArrayList = new ArrayList<LineSegment>(0);
         Point[] sortingArray = Arrays.copyOf(points, points.length);
+
         // Sort array by slope for each point
         for (int i = 0; i < points.length; i++) {
 
@@ -26,34 +28,52 @@ public class FastCollinearPoints {
 
             // traverse sub array to find segments.
             int state = 0; // 0 = looking, 1 = found segment
+            int maxPointIndex = 0;
             for (int j = 1; j < points.length; j++) {
                 if (points[i].slopeOrder().compare(sortingArray[j - 1], sortingArray[j]) == 0) {
                     StdOut.println("Matching slopes found");
-                    if (state == 0) state++; // Change state. Found segment
+                    if (state == 0) {
+                        state++; // Change state. Found segment
+                        if (points[i].compareTo(sortingArray[j - 1]) > 0)
+                            break;
+                        maxPointIndex = j - 1;
+                    }
 
                     // check that p is min. if not break
+                    if (points[i].compareTo(sortingArray[j]) > 0) break;
+                    StdOut.println("Good line segment found");
                     // stash the max point
+                    if (sortingArray[j].compareTo(sortingArray[maxPointIndex]) > 0)
+                        maxPointIndex = j;
                 }
                 else {  // Pattern is not found
-
-                    if (state == 1)
+                    if (state == 1) {
+                        // Here is where you stash the segment
+                        lineSegmentsArrayList
+                                .add(new LineSegment(points[i], sortingArray[maxPointIndex]));
                         break; // Leave because no more are found.
+                    }
                 }
             }
         }
-
-
-        // Low let's look at all base points
+        // Convert array list to segment array
+        StdOut.println("Does it ever get here?");
+        this.totalSegments = lineSegmentsArrayList.size();
+        this.validLineSegments = new LineSegment[lineSegmentsArrayList.size()];
+        // Convert the line segments Array list to array.
+        for (int i = 0; i < lineSegmentsArrayList.size(); i++) {
+            this.validLineSegments[i] = lineSegmentsArrayList.get(i);
+        }
     }
 
     // the number of line segments
     public int numberOfSegments() {
-        return 0;
+        return this.totalSegments;
     }
 
     // the line segments
     public LineSegment[] segments() {
-        return Arrays.copyOf(this.segs, 3);
+        return Arrays.copyOf(this.validLineSegments, 3);
     }
 
     public static void main(String[] args) {
