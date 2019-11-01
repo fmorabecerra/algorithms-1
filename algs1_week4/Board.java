ab2320487ec73ac1b80d3ef1dda0c45b213f82f6
Pancho <fmorabecerra@gmail.com>
2019-10-31 22:02:38 -0500

    Week 4: update board class to. Complete to string implementation. Simple to write. I don't know how efficient it is. May have to go back and iterate over 2d array.

--- algs1_week4/Board.java
+++ algs1_week4/Board.java
@@ -4,18 +4,30 @@
  *  Description: https://coursera.cs.princeton.edu/algs4/assignments/8puzzle/specification.php
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
 public class Board {
     private int n;
+    private int[][] currentBoard;
 
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
     public Board(int[][] tiles) {
         this.n = tiles.length;  // This may not be correct. Might be total number of elements.
+        // this.currentBoard = Arrays.copyOf(tiles, this.n);
+        this.currentBoard = tiles;
     }
 
     // string representation of this board
     public String toString() {
-        return "Hello";
+        String text = Arrays.deepToString(this.currentBoard);
+        // Remove all the stuff that you don't want
+        text = text.replace(", [", "\n").replace(",", "").replace("]", "").replace("[", "");
+        // Put everything together
+        return Integer.toString(this.n) + "\n" + text;
     }
 
     // board dimension n
@@ -56,6 +68,16 @@ public class Board {
     // unit testing (not graded)
     public static void main(String[] args) {
 
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] tiles = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                tiles[i][j] = in.readInt();
+        Board initial = new Board(tiles);
+
+        StdOut.println(initial.toString());
     }
 
 }
