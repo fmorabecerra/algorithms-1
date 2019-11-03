a7eb28290b06c972adb76948438b9af52f6cdeb5
Pancho <fmorabecerra@gmail.com>
2019-11-03 15:33:40 -0600

    Lots of clean up and proof that I can use priority queue.

--- algs1_week4/Solver.java
+++ algs1_week4/Solver.java
@@ -16,28 +16,11 @@ public class Solver {
     public Solver(Board initial) {
         if (initial == null) throw new IllegalArgumentException("null");
 
-        // class ManhattanOrder implements Comparator<Board> {
-        //     // @Override
-        //     public int compare(Board b1, Board b2) {
-        //         // return Double.compare(Point.this.slopeTo(p1), Point.this.slopeTo(p2));
-        //         return 0;
-        //     }
-        // }
-
-        // Comparator<Board> manhattanComparator = new Comparator<Board>() {
-        // {
-        //     @Override
-        //     public int compare(Board b1, Board b2) {
-        //         // return e1.getName().compareTo(e2.getName());
-        //         return 0;
-        //     }
-        // };
-
         this.priorityQueue = new MinPQ<SearchNode>();
         this.priorityQueue.insert(new SearchNode(initial));
-        // this.priorityQueue(initial.getClass().getMethod("manhattan", null));
-        // this.priorityQueue.insert(initial);
         this.minMoves = 0;
+
+        StdOut.println("Min board: " + this.priorityQueue.min().board);
     }
 
     // is the initial board solvable? (see below)
@@ -87,28 +70,8 @@ public class Solver {
 
 
     // My stuff
-    // private interface Comparator<Board> {
-    //     int compare(Board b1, Board b2);
-    // }
-    //
-    // private int compare(Board b1, Board b2) {
-    //     if (b1.equals(b2)) return 0;
-    //     return 0;
-    // }
-    // private Comparator<Board> manhattanOrder () {
-    //     /* YOUR CODE HERE */
-    //     return (new ManhattanOrder());
-    // }
 
-    // private class ManhattanOrder implements Comparator<Board> {
-    //     @Override
-    //     public int compare(Board b1, Board b2) {
-    //         // return Double.compare(Point.this.slopeTo(p1), Point.this.slopeTo(p2));
-    //         return 0;
-    //     }
-    // }
-
-    private class SearchNode { //implements Comparable<SearchNode> {
+    private class SearchNode {
         private Board board;
 
         public SearchNode(Board b) {
@@ -118,16 +81,5 @@ public class Solver {
         public int compareTo(SearchNode that) {
             return Integer.compare(this.board.manhattan(), that.board.manhattan());
         }
-        // public Comparator<SearchNode> slopeOrder() {
-        //     /* YOUR CODE HERE */
-        //     return (new manhatOrder());
-        // }
-
-        // private class manhatOrder implements Comparator<SearchNode> {
-        //     public int compare(SearchNode that) {
-        //         // return Double.compare(Point.this.slopeTo(p1), Point.this.slopeTo(p2));
-        //         return Integer.compare(SearchNode.this.board.manhattan(), that.board.manhattan());
-        //     }
-        // }
     }
 }
