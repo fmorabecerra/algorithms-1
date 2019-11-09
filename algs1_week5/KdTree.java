a0f94fb0e1b6b28be21eddbcd3edc09ddbeb955c
Pancho <fmorabecerra@gmail.com>
2019-11-09 17:23:18 -0600

    First successfull attempt at insertion.

--- algs1_week5/KdTree.java
+++ algs1_week5/KdTree.java
@@ -4,6 +4,7 @@
  *  Description: https://coursera.cs.princeton.edu/algs4/assignments/kdtree/specification.php
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 
@@ -30,6 +31,16 @@ public class KdTree {
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
         if (p == null) throw new IllegalArgumentException("Arg is null");
+
+        // Check if root is empty
+        if (this.rootNode == null) {
+            this.rootNode = new KdNode(p);
+            ++this.kdSize;
+            return;
+        }
+
+        // If root is not empty then start figuring out where to place the new node
+        compareNode(rootNode, new KdNode(p), true);
     }
 
     // does the set contain point p?
@@ -57,12 +68,54 @@ public class KdTree {
 
     // unit testing of the methods (optional)
     public static void main(String[] args) {
-
+        // initialize the two data structures with point from file
+        String filename = args[0];
+        In in = new In(filename);
+        // PointSET brute = new PointSET();
+        KdTree kdtree = new KdTree();
+        while (!in.isEmpty()) {
+            // for (int i = 0; i < 200; i++) {
+            double x = in.readDouble();
+            double y = in.readDouble();
+            Point2D p = new Point2D(x, y);
+            kdtree.insert(p);
+            // brute.insert(p);
+        }
     }
 
 
     // My stuff
+    private void compareNode(KdNode node, KdNode newNode, boolean compareHorizontalX) {
+        // Which direction should we take?
+        boolean goLeftOrBottom;
+        if (compareHorizontalX) {  // You may need to include this as part of the node. do not want to repeat.
+            if (newNode.point.x() < node.point.x()) {
+                goLeftOrBottom = true;
+            }
+            else {
+                goLeftOrBottom = false;
+            }
+        }
+        else {
+            if (newNode.point.y() < node.point.y()) { // How to deal with tie breakers
+                goLeftOrBottom = true;
+            }
+            else {
+                goLeftOrBottom = false;
+            }
+        }
 
+        // If child is empty then set here otherwise start search again
+        if (node.getChildNode(goLeftOrBottom) == null) {
+            node.setChildNode(goLeftOrBottom, newNode);
+            ++this.kdSize;
+        }
+        else {
+            compareNode(node.getChildNode(goLeftOrBottom), newNode, !compareHorizontalX);
+        }
+    }
+
+    // Private node class
     private static class KdNode {
         private Point2D point;
         private KdNode leftOrBottomNode;        // the left/bottom subtree
@@ -71,6 +124,22 @@ public class KdTree {
         public KdNode(Point2D p) {
             if (p == null) throw new IllegalArgumentException("Arg is null");
             this.point = p;
+            this.leftOrBottomNode = null;
+            this.rightOrTopNode = null;
+        }
+
+        public void setChildNode(boolean setLeftOrBottom, KdNode newNode) {
+            if (setLeftOrBottom)
+                this.leftOrBottomNode = newNode;
+            else
+                this.rightOrTopNode = newNode;
+        }
+
+        public KdNode getChildNode(boolean getLeftOrBottom) {
+            if (getLeftOrBottom)
+                return this.leftOrBottomNode;
+            else
+                return this.rightOrTopNode;
         }
     }
 }
